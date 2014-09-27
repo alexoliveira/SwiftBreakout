a5d3c146c177bbd123a97276c6f3d1f00dd4ebcd
Mauricio E. Bell'Albero <mauriciofmar3@gmail.com>
2014-09-27 15:09:08 -0700

    Ball own object

---
+++ SwiftBreakout/SwiftBreakout/Ball.swift
@@ -0,0 +1,13 @@
+//
+//  Ball.swift
+//  SwiftBreakout
+//
+//  Created by Mauricio Bell'Albero on 9/27/14.
+//  Copyright (c) 2014 swiftbreakout. All rights reserved.
+//
+
+import SpriteKit
+
+class Ball: SKSpriteNode {
+   
+}
