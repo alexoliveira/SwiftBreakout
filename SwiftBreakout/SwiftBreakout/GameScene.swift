a18d91331aa4ed9488e0fc05a25d5b4dfd25cfaf
Alexandre Oliveira <alex@storytree.me>
2014-09-27 19:51:13 -0700

    Adding explosion when brick is hit.

--- SwiftBreakout/SwiftBreakout/GameScene.swift
+++ SwiftBreakout/SwiftBreakout/GameScene.swift
@@ -11,8 +11,11 @@ import SpriteKit
 let paddleCategory : UInt32 = 1 << 0
 let ballCategory : UInt32 = 1 << 1
 let brickCategory : UInt32 = 1 << 2
+let explosionCategory : UInt32 = 1 << 3
 
 class GameScene: SKScene, SKPhysicsContactDelegate {
+    var bricksToExplode : [SKNode] = []
+    
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
         self.physicsWorld.gravity = CGVector(0.0, 0.0)
@@ -54,16 +57,21 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
    
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
+        if bricksToExplode.count > 0 {
+            explodeBricks()
+        }
     }
     
     func generateBricks() {
         let generator = BricksGenerator(scene: self)
         generator.createDefaultBricksPattern()
     }
+
     func didBeginContact(contact: SKPhysicsContact) {
         var contactBodies = [contact.bodyA.node, contact.bodyB.node]
         for node in contactBodies {
             if node?.name == "Brick" {
+                bricksToExplode.append(node!)
                 node?.removeFromParent()
             }
         }
@@ -84,5 +92,32 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
             myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
             self.addChild(myLabel)
         }
+    }
+
+    func explodeBricks() {
+        println("exploding brick")
+        let brickToExplode = bricksToExplode.removeLast()
+        let explosionIconNames = ["no_semicolons", "swift icon", "poo"]
+        let explosionIconIndex = Int(rand()) % (explosionIconNames.count)
+        let explosionParticle = SKSpriteNode(imageNamed: explosionIconNames[explosionIconIndex])
+        if let size = explosionParticle.texture?.size() {
+            explosionParticle.physicsBody = SKPhysicsBody(texture: explosionParticle.texture, size: size)
+            explosionParticle.position = brickToExplode.position
+            println(brickToExplode.position)
+//            explosionParticle.physicsBody?.mass = 1
+            explosionParticle.physicsBody?.restitution = 1.0
+            explosionParticle.physicsBody?.friction = 0.0
+            explosionParticle.physicsBody?.linearDamping = 0.0
+            explosionParticle.physicsBody?.allowsRotation = false
+//            explosionParticle.physicsBody?.dynamic = false
+            explosionParticle.physicsBody?.applyAngularImpulse(0.01)
+            explosionParticle.name = "Explosion Particle"
+            
+            explosionParticle.physicsBody?.categoryBitMask = explosionCategory
+            explosionParticle.physicsBody?.collisionBitMask = 0
+            explosionParticle.physicsBody?.contactTestBitMask = 0
+        }
+        self.addChild(explosionParticle)
+        explosionParticle.physicsBody?.applyImpulse(CGVector(0.0, -10.0))
     }
 }
